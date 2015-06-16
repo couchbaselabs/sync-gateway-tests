7d4f2abd33c8779bd63e1ab5599133d18a10782c
Andrei Baranouski <andrei.baranouski@gmail.com>
2015-06-17 02:52:55 +0300

    cbl-replication-mismatch-restart-cb.js: aded new verification step:
    verify sync gateway changes feed has all docs in it
    
    https://github.com/couchbase/sync_gateway/issues/750

--- tests/cbl-replication-mismatch-restart-cb.js
+++ tests/cbl-replication-mismatch-restart-cb.js
@@ -203,6 +203,53 @@ test("verify cbl changes on dbs[1]", function(t){
     })
 })
 
+test("verify sync gateway changes feed has all docs in it", test_conf, function(t) {
+    var db = coax(sg1.db.pax().toString())
+
+    db("_changes", function (err, data) {
+        console.log(data)
+        var changes = data.results.map(function(r){return r.id});
+        db("_all_docs", function(err, view){
+            var docs = view.rows;
+            var missing = [];
+
+            docs.forEach(function(d){
+                if (changes.indexOf(d.id) == -1) {
+                    missing.push(d.id)
+                }
+            })
+
+            var changeIds = {}, dupIds = [];
+            var changeSeqs = {}, dupSeqs = [];
+
+            data.results.forEach(function(r){
+                if (changeIds[r.id]) {
+                    dupIds.push(r.id)
+                }
+                changeIds[r.id] = true
+
+                if (changeSeqs[r.seq]) {
+                    dupSeqs.push(r.seq)
+                }
+                changeSeqs[r.seq] = true
+            })
+
+            t.equals(docs.length, numDocs*3/2, "correct number of docs in _all_docs:" + docs.length )
+            //t.equals(changes.length, numDocs + 1, "correct number of docs in _changes:" + changes.length)
+            console.log("missing " + missing.length + ", ids:", missing.join(', '))
+            console.log("duplicate change ids "+dupIds.length+", ids:", dupIds.join(', '))
+            console.log("duplicate change seqs "+dupSeqs.length+", seqs:", dupSeqs.join(', '))
+
+            t.equals(dupIds.length, 0, "duplicate ids in changes:"+ dupIds.length)
+            t.equals(dupSeqs.length, 0, "duplicate seqs in changes:" + dupSeqs.length)
+            t.equals(missing.length, 0, "missing changes:" + missing.length)
+
+
+            t.end()
+        })
+
+    })
+})
 
 test("cleanup cb bucket", function(t){
     if (config.DbUrl.indexOf("http") > -1){
