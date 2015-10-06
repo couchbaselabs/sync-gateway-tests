410bd51c41770bd8cc2cddeb637fea38c53a585f
Andrei Baranouski <andrei.baranouski@gmail.com>
2015-10-06 09:53:21 +0300

    NUM_REVS = 10 by default for android

--- tests/a-cb-presetup.js
+++ tests/a-cb-presetup.js
@@ -1,6 +1,5 @@
 var launcher = require("../lib/launcher"),
-    ini = require("ini"),
-    fs = require("fs"),
+    spawn = require('child_process').spawn
     common = require("../tests/common"),
     conf_file = process.env.CONF_FILE || 'local',
     cb_util = require("./utils/cb_util"),
@@ -17,10 +16,16 @@ test("delete buckets", test_conf, function (t) {
                 t.end()
             }, 40000));
     } else {
-        t.end()
+        t.end();
     }
 });
 
+test("kill LiteServ on android", test_conf, function (t) {
+    if (config.provides == "android") {
+        spawn('adb', ["shell", "am", "force-stop", "com.couchbase.liteservandroid"]);
+        t.end();
+    }
+});
 
 test("create buckets", test_conf, function (t) {
     if (config.DbUrl.indexOf("http") > -1) {
@@ -28,42 +33,6 @@ test("create buckets", test_conf, function (t) {
             t.end();
         }, 40000));
     } else {
-        t.end()
+        t.end();
     }
 });
-
-
-/*(test("create bucket", test_conf,
-    function(t) {
-        if (config.DbUrl.indexOf("http") > -1) {
-            var ini_file_config = ini.parse(fs.readFileSync(config.cluster_ini_file, "utf-8"));
-
-            var user = "Administrator" // ini_file_config.global.rest_username;
-            var password = "password" // ini_file_config.global.rest_password;
-            var servers = ini_file_config.servers;
-            var bucket = ini_file_config.bucket.bucket1;
-            var server = "localhost" // servers.vm1.substring(0,
-                // servers.vm1.indexOf(":"));
-            var port = 8091 // servers.vm1.substring(servers.vm1.indexOf(":") +
-                // 1);
-
-            var post_data = "name=" + bucket + "&parallelDBAndViewCompaction=false&autoCompactionDefined=false&threadsNumber=3&replicaIndex=0&replicaNumber=1&saslPassword=&authType=sasl&ramQuotaMB=200&bucketType=membase&flushEnabled=1";
-            var options = {
-                host: server,
-                port: port,
-                path: '/pools/default/buckets',
-                method: 'POST',
-                auth: user + ":" + password,
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Content-Length': post_data.length
-                }
-            };
-            common.http_post_api(t, post_data, options, undefined, function (callback) {
-                t.end()
-            }, 20000);
-        } else {
-            t.end();
-        };
-
-    });*/
