9de788bccab4a9ad4f0e0ed0ed2e598bf5b55f89
ashvindersingh <ashvindersingh@gmail.com>
2016-01-28 19:53:37 -0800

    run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -10,7 +10,7 @@ do
   python ./upgrade_test/create_buckets.py $1 $3
   sleep 15
   echo $file
-  node "$file" | tee results.tap 
+  node "$file" | tee -a results.tap 
   sleep 5
   rm -rf *.pindex
   python ./upgrade_test/delete_buckets.py $1 $2
