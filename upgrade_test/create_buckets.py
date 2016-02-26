847e3ab98816eaa4bfc5b7fea2b745f7ce10c017
ashvindersingh <ashvindersingh@gmail.com>
2016-02-26 14:53:56 -0800

    use sasl buckets

--- upgrade_test/create_buckets.py
+++ upgrade_test/create_buckets.py
@@ -4,12 +4,13 @@ import sys
 
 def create_bucket(ip,name):
     ip_addr = 'http://'+ip+':8091/pools/default/buckets'
-    payload = {"name":name,"ramQuotaMB":"1024","authType":"none","proxyPort":11217,"bucketType":"couchbase"}
+    payload = {"name":name,"ramQuotaMB":"1024","authType":"sasl","proxyPort":"11211","bucketType":"couchbase","replicaNumber":"2"}
     r = requests.post(ip_addr, data = payload,auth=('Administrator', 'password'))
     
     if r.status_code == 202:
         print "Success: Created the bucket with Default name"
     else:
+	print "Bucket creation failed!", r.status_code
         sys.exit(1)
 
 ip = sys.argv[1]
