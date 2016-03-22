7f41ece47369eabd253e55d5e63ffa4ac97d9292
Seth Rosetter <seth.rosetter@gmail.com>
2016-03-21 20:19:53 -0700

    Add support for launching API 21 and API 15 emulators

--- start_emulator.sh
+++ start_emulator.sh
@@ -6,8 +6,10 @@ apiversion=$1
 
 if [ "$apiversion" == "23" ]; then
     emulator -scale 0.25 @Nexus_5_API_23_x86 &
+elif [ "$apiversion" == "21" ]; then
+    emulator -scale 0.5 @Nexus_5_API_21_x86_64 &
 elif [ "$apiversion" == "15" ]; then
-    emulator -scale 0.5 @4_7_WXGA_API_15 &
+    emulator -scale 0.5 @Nexus_5_API_15_x86 &
 else
     echo "Unsupported API version. Did not launch emulator"
     echo "Usage: './start_emulator.sh 15'"
