c90cb254be51cb6f97b1b9719d50d2be2af3f261
Seth Rosetter <seth.rosetter@gmail.com>
2016-03-30 10:50:50 -0700

    Fix comment

--- start_emulator.sh
+++ start_emulator.sh
@@ -10,7 +10,7 @@ elif [ "$apiversion" == "21" ]; then
     emulator -scale 0.25 @Nexus_5_API_21_x86_64 &
 elif [ "$apiversion" == "19-armeabi-v7a" ]; then
     emulator -scale 0.25 @Nexus_5_API_19_armeabi-v7a &
-    echo "Waiting 2 min ..."
+    echo "Waiting 5 min ..."
     sleep 300
 elif [ "$apiversion" == "15" ]; then
     emulator -scale 0.25 @Nexus_5_API_15_x86 &
