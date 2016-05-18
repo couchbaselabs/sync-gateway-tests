40d7d469ecb39a3ae1f3c28f6301de59605ea3ba
Seth Rosetter <seth.rosetter@gmail.com>
2016-05-18 15:32:35 -0700

    Fix comment

--- config/local_cb.js
+++ config/local_cb.js
@@ -18,7 +18,7 @@ var config = module.exports = {
  * the bucket db must be created manually with enable flush in advance
  * 
  * database information in this file will override the values in this config.
- * the default admin_party_cb.json will use "http://localhost:8091" on bucket "db"
+ * the default admin_party_cb.json will use "http://192.168.33.10:8091" on bucket "db"
  */
 module.exports.SyncGatewayAdminParty = __dirname+"/admin_party_cb.json"
 module.exports.SyncGatewaySyncFunctionTest = __dirname+"/sync_function_test.json"
