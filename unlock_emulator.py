14c827e3aca9ebbb66f4445cf1744999ff4d51f3
Seth Rosetter <seth.rosetter@gmail.com>
2016-06-07 12:42:08 -0700

    Add wait for device

--- unlock_emulator.py
+++ unlock_emulator.py
@@ -9,6 +9,8 @@ from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
 if __name__ == '__main__':
 
-    print('Unlocking emulator ...')
+    print('Waiting for device "emulator-5554"')
+    device = MonkeyRunner.waitForConnection(timeout=300, deviceId="emulator-5554")
 
+    print('Unlocking emulator ...')
     device.press('KEYCODE_MENU', MonkeyDevice.DOWN_AND_UP)
