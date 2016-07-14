e359c56afa23e2bae412f3c6e08d8a6c927f83a5
Seth Rosetter <seth.rosetter@gmail.com>
2016-07-14 11:25:58 -0700

    Scale is deprecated when starting emulator

--- start_emulator.sh
+++ start_emulator.sh
@@ -5,19 +5,19 @@
 apiversion=$1
 
 if [ "$apiversion" == "23" ]; then
-    emulator -scale 0.25 @Nexus_5_API_23_x86 &
+    emulator @Nexus_5_API_23_x86 &
 elif [ "$apiversion" == "21" ]; then
-    emulator -scale 0.25 @Nexus_5_API_21_x86_64 &
+    emulator @Nexus_5_API_21_x86_64 &
 elif [ "$apiversion" == "19" ]; then
-    emulator -scale 0.25 @Nexus_5_API_19_x86 &
+    emulator @Nexus_5_API_19_x86 &
 elif [ "$apiversion" == "19-armeabi-v7a" ]; then
-    emulator -scale 0.25 @Nexus_5_API_19_armeabi-v7a &
+    emulator @Nexus_5_API_19_armeabi-v7a &
     echo "Waiting 5 min ..."
     sleep 300
 elif [ "$apiversion" == "15" ]; then
-    emulator -scale 0.25 @Nexus_5_API_15_x86 &
+    emulator @Nexus_5_API_15_x86 &
 elif [ "$apiversion" == "15-armeabi-v7a" ]; then
-    emulator -scale 0.25 @Nexus_5_API_15_armeabi-v7a &
+    emulator @Nexus_5_API_15_armeabi-v7a &
 else
     echo "Unsupported API version. Did not launch emulator"
     echo "Usage: './start_emulator.sh 15'"
