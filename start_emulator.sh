b58c19dd4d681f3baf8c37334fbae756fed12f1a
Seth Rosetter <seth.rosetter@gmail.com>
2016-07-22 18:08:13 -0700

    Update start_emulator.sh

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-# Requires that Nexus_5_API_23_x86 and 4_7_WXGA_API_15 avds have been set up.
+# Requires that avds have been set up.
 
 apiversion=$1
 
-if [ "$apiversion" == "23" ]; then
+if [ "$apiversion" == "24" ]; then
+    emulator @Nexus_6_API_24_x86 &
+elif [ "$apiversion" == "23" ]; then
     emulator @Nexus_5_API_23_x86 &
-elif [ "$apiversion" == "21" ]; then
-    emulator @Nexus_5_API_21_x86_64 &
-elif [ "$apiversion" == "19" ]; then
-    emulator @Nexus_5_API_19_x86 &
+elif [ "$apiversion" == "22" ]; then
+    emulator @Nexus_5_API_22_x86 &
 elif [ "$apiversion" == "19-armeabi-v7a" ]; then
     emulator @Nexus_5_API_19_armeabi-v7a &
     echo "Waiting 5 min ..."
     sleep 300
 elif [ "$apiversion" == "16" ]; then
     emulator @Nexus_4_API_16_x86 &
-elif [ "$apiversion" == "15" ]; then
-    emulator @Nexus_5_API_15_x86 &
-elif [ "$apiversion" == "15-armeabi-v7a" ]; then
-    emulator @Nexus_5_API_15_armeabi-v7a &
 else
     echo "Unsupported API version. Did not launch emulator"
     echo "Usage: './start_emulator.sh 15'"
