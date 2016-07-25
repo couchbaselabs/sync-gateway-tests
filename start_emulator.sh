4cc817586dd7de9fd0036e4b17a1e99f3d3d5f69
Seth Rosetter <seth.rosetter@gmail.com>
2016-07-25 10:07:39 -0700

    Update emulator launcher

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Requires that avds have been set up.
+# Requires that Nexus_5_API_23_x86 and 4_7_WXGA_API_15 avds have been set up.
 
 apiversion=$1
 
-if [ "$apiversion" == "24" ]; then
-    emulator @Nexus_6_API_24_x86 &
-elif [ "$apiversion" == "23" ]; then
+if [ "$apiversion" == "23" ]; then
     emulator @Nexus_5_API_23_x86 &
-elif [ "$apiversion" == "22" ]; then
-    emulator @Nexus_5_API_22_x86 &
+elif [ "$apiversion" == "21" ]; then
+    emulator @Nexus_5_API_21_x86 &
+elif [ "$apiversion" == "19" ]; then
+    emulator @Nexus_5_API_19_x86 &
 elif [ "$apiversion" == "19-armeabi-v7a" ]; then
     emulator @Nexus_5_API_19_armeabi-v7a &
     echo "Waiting 5 min ..."
