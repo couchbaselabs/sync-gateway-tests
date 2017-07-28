d97b62410a4bfae9d82252d8ba38cd2924e31ddb
Seth Rosetter <seth.rosetter@gmail.com>
2017-07-28 10:49:48 -0700

    Fix auth for bucket flush

--- config/local_cb.js
+++ config/local_cb.js
@@ -6,7 +6,7 @@ var config = module.exports = {
   SyncGatewayPath   : process.env.SYNCGATE_PATH,
   LocalListenerIP   : process.env.LOCAL_IP || "127.0.0.1",
   LocalListenerPort : 8189,
-  DbUrl             : "http://s61405cnt72.sc.couchbase.com:8091",
+  DbUrl             : "http://Administrator:password@s61405cnt72.sc.couchbase.com:8091",
   DbBucket          : "db",
   provides          : "ios",  // ios, android, pouchdb, couchdb
   storageEngine     : process.env.STORAGE_ENGINE || "SQLite",
@@ -19,7 +19,7 @@ var config = module.exports = {
  * the bucket db must be created manually with enable flush in advance
  * 
  * database information in this file will override the values in this config.
- * the default admin_party_cb.json will use "http://s61405cnt72.sc.couchbase.com:8091" on bucket "db"
+ * the default admin_party_cb.json will use "http://Administrator:password@s61405cnt72.sc.couchbase.com:8091" on bucket "db"
  */
 module.exports.SyncGatewayAdminParty = __dirname+"/admin_party_cb.json"
 module.exports.SyncGatewaySyncFunctionTest = __dirname+"/sync_function_test.json"
